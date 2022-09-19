b0c061a610101442464a958551eccbb527acfb8c
Vangeanc3 <ismaelmartins919@gmail.com>
2022-09-19 01:51:39 -0300

    first version

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let burgue = document.querySelector(".hamburgue")
+let menu = document.querySelector(".menu")
+
+burgue.addEventListener("click", () =>{
+    // menu.style.right = "0"
+    menu.classList.toggle("menu-toggle")
+})

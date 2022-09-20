6c4c5d322101e10041b8a0d1a4b9292ada60187f
Ismael Cardoso Martins <est_imartins@banparanet.com.br>
2022-09-20 16:41:56 -0300

    v3

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 let burgue = document.querySelector(".hamburgue")
 let menu = document.querySelector(".menu")
 
+var loadingAnimation = document.querySelector(".loading")
 var percent = document.querySelector(".percent");
 var progress = document.querySelector(".progress");
 var container = document.querySelector(".loader");
 var count = 4;
 var per = 16;
-var loading = setInterval(animate, 50);
+var loading = setInterval(animate, 30);
 
 
 // MenuToglle
 burgue.addEventListener("click", () =>{
-    // menu.style.right = "0"
     menu.classList.toggle("menu-toggle")
 })
 
@@ -19,6 +19,7 @@ burgue.addEventListener("click", () =>{
 function animate(){
     if(count == 100 && per == 400){
         clearInterval(loading);
+        loadingAnimation.classList.add("loading-animation")
         container.classList.add("loader-animation")
     } else{
         per = per + 4;

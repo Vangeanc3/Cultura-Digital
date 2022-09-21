0925be5b350f37ed99ea5b609265e11539220941
Vangeanc3 <ismaelmartins919@gmail.com>
2022-09-21 01:27:22 -0300

    v4

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ var count = 4;
 var per = 16;
 var loading = setInterval(animate, 30);
 
+var body = document.querySelector("body")
 
 // MenuToglle
 burgue.addEventListener("click", () =>{
@@ -22,6 +23,7 @@ function animate(){
         loadingAnimation.classList.add("loading-animation")
         container.classList.add("loader-animation")
     } else{
+        body.style.overflowY = "hidden";
         per = per + 4;
         count = count + 1;
         progress.style.width = per + "px";
